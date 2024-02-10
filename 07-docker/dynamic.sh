43416da487d41e3d18deb0886840292da8eb6114
L Ilua <theykilledteddybear@gmail.com>
2024-02-10 11:55:32 +0300

    Dockerfile echo rewrite, delete unused directory

--- 07-docker/dynamic.sh
+++ 07-docker/dynamic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t hometask-image -f Dockerfile .
-docker run -d --rm --name hometask-dynamic -p 8080:80 hometask-image
+docker run  -d --rm --name hometask-dynamic -p 8080:80 hometask-image
