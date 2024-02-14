8cb4b98a32212b404378709e75b4481aa50286ce
L Ilua <theykilledteddybear@gmail.com>
2024-02-14 22:29:05 +0300

    Fix static.sh file

--- 07-docker/static.sh
+++ 07-docker/static.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 docker run -v ./files:/var/www/dynamic/ -d --name hometask-static  -p 8081:80 hometask-image
-docker exec hometask-static chown www-data:www-data /var/www/dynamic/index.html
