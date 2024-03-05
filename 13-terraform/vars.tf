ba269bca8c2e9c3b18cbaf972737b3a32ced86f9
IluaRoot <142495530+IluaRoot@users.noreply.github.com>
2024-03-05 18:06:32 +0300

    13-terraform push hometask

---
+++ 13-terraform/vars.tf
@@ -0,0 +1,38 @@
+variable "replica_num" {}
+variable "service_port" {}
+variable "service_name" { type = string }
+variable "service_target_port" { type = number }
+variable "github_token" { type = string }
+variable "repo_var" { type = string }
+variable "git_branch" { type = string }
+
+variable "container_name" {
+  type    = string
+  default = "wcg-depl-terra-v1"
+}
+
+variable "label_name" {
+  type    = string
+  default = "wcg-depl-terra-v1"
+}
+
+variable "image_name" {
+  type    = string
+  default = "ghcr.io/iluaroot/wcg:latest"
+}
+
+variable "docker_image" {
+  type    = string
+  default = "wcg:latest"
+}
+
+variable "ingress_name" {
+  type    = string
+  default = "wcg-app-ingress"
+}
+
+variable "ingress_hostname" {
+  type    = string
+  default = "wcg-app-terraform.io"
+
+}
