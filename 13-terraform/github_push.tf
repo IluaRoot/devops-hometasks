afc32dce4755bb7e2acdbbe57bd0f770552a8073
IluaRoot <142495530+IluaRoot@users.noreply.github.com>
2024-03-06 22:02:37 +0300

    Fin ingress rewrite root

--- 13-terraform/github_push.tf
+++ 13-terraform/github_push.tf
@@ -1,16 +1,16 @@
 resource "github_repository_file" "name" {
   for_each = { file1 = "config.tf",
-               file2 = "github_push.tf",
-               file3 = "terraform.tfvars",
-               file4 = "vars.tf",
-               file5 = "wcg_deploy.tf",
-               file6 = "wcg_ingress.tf",
-               file7 = "wcg_service_port.tf" }
+    file2 = "github_push.tf",
+    file3 = "terraform.tfvars",
+    file4 = "vars.tf",
+    file5 = "wcg_deploy.tf",
+    file6 = "wcg_ingress.tf",
+  file7 = "wcg_service_port.tf" }
 
-  repository = var.repo_var
-  branch     = var.git_branch
-  file       = "13-terraform/${each.value}"
-  content    = file("${each.value}")
+  repository     = var.repo_var
+  branch         = var.git_branch
+  file           = "13-terraform/${each.value}"
+  content        = file("${each.value}")
   commit_message = var.commit_msg
 
   overwrite_on_create = true
