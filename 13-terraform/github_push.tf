c52fbf43e2b55e76cdcf5b5889a668a894f61502
Ilua L <theykilledteddybear@gmail.com>
2024-03-06 22:15:05 +0300

    Fix stuff in 13 task

--- 13-terraform/github_push.tf
+++ 13-terraform/github_push.tf
@@ -5,13 +5,17 @@ resource "github_repository_file" "name" {
     file4 = "vars.tf",
     file5 = "wcg_deploy.tf",
     file6 = "wcg_ingress.tf",
-  file7 = "wcg_service_port.tf" }
+    file7 = "wcg_service_port.tf",
+  file8 = "README.md" }
 
   repository     = var.repo_var
   branch         = var.git_branch
   file           = "13-terraform/${each.value}"
   content        = file("${each.value}")
   commit_message = var.commit_msg
+  commit_author = var.commit_auth
+  commit_email = var.commit_mail
+
 
   overwrite_on_create = true
 }
