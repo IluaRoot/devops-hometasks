ed50b7084b6ab31917d85db11e04067b5f6ce241
IluaRoot <142495530+IluaRoot@users.noreply.github.com>
2024-03-06 12:15:50 +0300

    13-terraform push hometask

--- 13-terraform/github_push.tf
+++ 13-terraform/github_push.tf
@@ -1,9 +1,11 @@
-#resource "github_repository" "wcg-push-repo" {
-#  name      = var.repo_var
-#}
-
 resource "github_repository_file" "name" {
-  for_each = { file1 = "config.tf", file2 = "github_push.tf", file3 = "terraform.tfvars", file4 = "vars.tf", file5 = "wcg_deploy.tf", file6 = "wcg_ingress.tf", file7 = "wcg_service_port.tf" }
+  for_each = { file1 = "config.tf",
+               file2 = "github_push.tf",
+               file3 = "terraform.tfvars",
+               file4 = "vars.tf",
+               file5 = "wcg_deploy.tf",
+               file6 = "wcg_ingress.tf",
+               file7 = "wcg_service_port.tf" }
 
   repository = var.repo_var
   branch     = var.git_branch
