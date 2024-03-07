952f95082db52acb268dbd99281940779a8e9efe
Ilua L <theykilledteddybear@gmail.com>
2024-03-07 19:02:51 +0300

    add kubernetes backend

--- 13-terraform/config.tf
+++ 13-terraform/config.tf
@@ -8,6 +8,11 @@ terraform {
       source  = "integrations/github"
       version = "~> 4.0"
     }
+    
+  }
+  backend "kubernetes" {
+    secret_suffix = "state"
+    config_path   = "~/.kube/config"
   }
 }
 
